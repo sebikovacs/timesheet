fc204abdd1a45960badd590265aa7617db60a407
Sebi Kovacs <sebi.kovacs@gmail.com>
2012-05-09 16:16:58 +0300

    added files for the app

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>Timesheet</title>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+	<script type="text/javascript" src="js/jquery.hotkeys.js"></script>
+	<script type="text/javascript" src="js/app.js"></script>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/stylesheet.css">
+	<link rel="icon" href="http://timesheet.dev/favicon.ico" type="image/x-icon" />
+</head>
+<body>
+	
+	<div class="container">
+		<h1>Timesheet</h1>
+		<div class="row">
+			<div class="alert alert-info span4 offset4">
+				<p>The system needs your permision to notify when the it's time to fill in your timesheet. Please push the button to do so:</p>
+				<a href="" title="" class="btn permit-notifications btn-warning">Permit notifications?</a>
+			</div>
+		</div>
+		<div class="row content">
+			
+				<form action="">
+					<fieldset>
+						<label>'Sup?</label>
+						<textarea name="comment" id="" cols="30" rows="10"></textarea>
+						<!--<button type="submit" class="right btn btn-success">Submit</button>-->
+					</fieldset>
+				</form>
+			
+			<div class="span6">
+				<ul class="unstyled">
+				</ul>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
---
+++ js/app.js
@@ -0,0 +1,164 @@
+//notify object, src: 0xfe.muthanna.com/notifyme.html
+
+function Notifier() {}
+
+// Returns "true" if this browser supports notifications.
+Notifier.prototype.HasSupport = function() {
+  if (window.webkitNotifications) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Request permission for this page to send notifications. If allowed,
+// calls function "cb" with true.
+Notifier.prototype.RequestPermission = function(cb) {
+  window.webkitNotifications.requestPermission(function() {
+    if (cb) { cb(window.webkitNotifications.checkPermission() == 0); }
+  });
+}
+
+// Popup a notification with icon, title, and body. Returns false if
+// permission was not granted.
+Notifier.prototype.Notify = function(icon, title, body) {
+    if (window.webkitNotifications.checkPermission() == 0) {
+    	var popup = window.webkitNotifications.createNotification(icon, title, body);
+    	popup.show();
+        return true;
+    }
+  	return false;
+}
+
+var App = {
+	init: function(config){
+		this.config = config;
+		this.bindEvents();
+		this.startDb();
+		this.readDb();
+		this.notifications();
+	},
+	notifications: function(){
+		var nc = new Notifier;
+		
+		if (nc.HasSupport() ){
+			$('.alert-info').hide();
+			setInterval(function(){
+				var date = new Date();
+				var hours = date.getHours();
+				var minutes = date.getMinutes();
+
+				(hours < 10) ? hours = '0'+ hours : hours = hours;
+				(minutes < 10) ? minutes = '0'+ minutes : minutes = minutes;
+
+				if (hours == 12 && minutes == 00){
+					nc.Notify('','Timesheet', 'Completeaza timesheet-ul');
+				}
+				if (hours == 16 && minutes == 00){
+					nc.Notify('','Timesheet', 'Completeaza timesheet-ul');
+				}
+
+				if (hours == 17 && minutes == 45){
+					nc.Notify('','Timesheet', 'Completeaza timesheet-ul');
+				}
+
+			}, 60000);
+		}
+	},
+	startDb: function(){
+		this.db = openDatabase('timesheet', '1.0', 'my first database', 2 * 1024 * 1024);
+		this.db.transaction(function (tx) {
+			//tx.executeSql('drop table timesheet');
+		  	tx.executeSql('CREATE TABLE IF NOT EXISTS timesheet (id INTEGER PRIMARY KEY AUTOINCREMENT, data datetime , text varchar(255))');
+		  	
+		});
+	},
+	readDb:function () {
+		var self = App;
+		self.db.transaction(function (tx) {
+			tx.executeSql('SELECT * FROM timesheet', [], function (tx, results) {
+				var len = results.rows.length, i;
+				var $ul = $('ul');
+				
+				//console.log(results.rows.item(0));
+				for (i = 0; i < len; i++) {
+					var $li = $('<li>');
+					$li.attr('class','clearfix');
+					
+					var $date = $('<strong>').html(results.rows.item(i).data);
+					var $description = $('<span>').html(results.rows.item(i).text);
+					
+					$li.append($date);
+					$li.append($description);
+					$ul.prepend($li);
+
+					//alert(results.rows.item(i).text);
+				}
+			});
+		});
+	},
+	bindEvents: function(){
+		var self = App;
+		$('button.btn').on('click', this.onSubmit);
+		$('a.permit-notifications').on('click', this.onNotificationRequest);
+		$('textarea').bind('keydown','ctrl+return', this.onSubmit);
+	},
+	onSubmit: function(e){
+		var self = App;
+		var $textarea = $('textarea');
+		var content = $textarea.val();
+		var date = new Date();
+		var year = date.getFullYear().toString();
+		var month = date.getMonth()+1;
+		var day = date.getDate().toString();
+
+		if (month < 10) {
+			month = '0' + month.toString();
+		}
+		if (day < 10) {
+			day = '0' + day;
+		}
+		var dateString = day + '/' + month + '/' + year;
+
+		//validate textarea
+		if (content === ''){
+			alert('Please fill in the textbox with a status');
+			return false;
+		}
+
+		//prepend information to list
+		var $ul = $('ul');
+		var $li = $('<li>');
+		$li.attr('class','clearfix');
+
+		var $date = $('<strong>').html(dateString);
+		var $description = $('<span>').html(content);
+		
+		//clear textarea
+		
+
+		//append information
+		$li.append($date);
+		$li.append($description);
+		$ul.prepend($li);
+		$textarea.val('');
+
+		//save to database
+		self.db.transaction(function (tx) {
+			tx.executeSql('INSERT INTO timesheet ( data, text) VALUES (?,?)', [dateString , content]);
+		});
+
+		e.preventDefault();
+	},
+	onNotificationRequest: function(e){
+		var self = App;
+		var nc = new Notifier;
+		nc.RequestPermission();
+
+		e.preventDefault();
+
+	}
+}
+jQuery(document).ready(function(){
+	App.init();
+});
